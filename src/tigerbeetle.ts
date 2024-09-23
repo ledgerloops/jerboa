9f5bc4d5acb69b2378d5a78f6d772a2e07421876
Michiel de Jong <michiel@pondersource.com>
2024-09-23 12:07:17 +0200

    BigInt amount

--- src/tigerbeetle.ts
+++ src/tigerbeetle.ts
@@ -1,11 +1,14 @@
-import { CreateAccountError, createClient } from 'tigerbeetle-node';
+import { CreateAccountError, createClient, id } from 'tigerbeetle-node';
 
 export class TigerBeetleStores {
     client;
     constructor() {
     }
-    async ensureBalance(thisParty: string, otherParty: string): Promise<void> {
-      const id = BigInt(1000 * 1000 * parseInt(thisParty) + parseInt(otherParty));
+    async ensureBalance(thisParty: string, otherParty: string): Promise<{ thisPartyId: bigint, otherPartyId: bigint}> {
+      const thisPartyId = BigInt(isNaN(parseInt(thisParty)) ? 0 : parseInt(thisParty));
+      const otherPartyId = BigInt(isNaN(parseInt(otherParty)) ? 0 : parseInt(otherParty));
+      
+      const id = BigInt(1000 * 1000) * thisPartyId + otherPartyId;
       const account = {
         id,
         debits_pending: 0n,
@@ -22,8 +25,17 @@ export class TigerBeetleStores {
         timestamp: 0n,
       };
       
-      const accountErrors = await this.client.createAccounts([account]);
-      console.log('account created', accountErrors, CreateAccountError["exists"]);
+      const accountErrors: { index: number, result: number}[] = await this.client.createAccounts([account]);
+      accountErrors.forEach(({ index, result }: { index: number, result: number})  => {
+        if (result !== CreateAccountError["exists"]) {
+          throw new Error(`error creating TigerBeetle account ${index} ${result}`);
+        }
+      });
+      // console.log('account created', accountErrors, );
+      return {
+        thisPartyId,
+        otherPartyId
+      }
   }
     async connect(): Promise<void> {
       this.client = createClient({
@@ -36,8 +48,44 @@ export class TigerBeetleStores {
       // noop
   }
     async storeTransaction({ thisParty, otherParty, amount }: { thisParty: string, otherParty: string, amount: number }): Promise<number> {
-      this.ensureBalance(thisParty, otherParty);
+      const firstChunk = Math.round(Math.abs(amount));
+      const afterFirst = 1000 * 1000 * (amount - firstChunk);
+      const secondChunk = Math.round(afterFirst);
+      const afterSecond = 1000 * 1000 * (afterFirst - secondChunk);
+      const lastChunk = Math.round(afterSecond);
+
+      const scaledAmount =
+        BigInt(firstChunk) * BigInt(1000 * 1000) * BigInt(1000 * 1000) +
+        BigInt(secondChunk) * BigInt(1000 * 1000) +
+        BigInt(lastChunk);
+
+      const { thisPartyId, otherPartyId } = await this.ensureBalance(thisParty, otherParty);
       // this.balances[thisParty][otherParty] += amount;
+      let debit_account_id, credit_account_id;
+      if (amount >= 0) {
+        debit_account_id = thisPartyId;
+        credit_account_id = otherPartyId;
+      } else {
+        debit_account_id = otherPartyId;
+        credit_account_id = thisPartyId;
+      }
+      const transfers = [{
+        id: id(),
+        debit_account_id,
+        credit_account_id,
+        amount: scaledAmount,
+        pending_id: 0n,
+        user_data_128: 0n,
+        user_data_64: 0n,
+        user_data_32: 0,
+        timeout: 0,
+        ledger: 1,
+        code: 720,
+        flags: 0,
+        timestamp: 0n,
+      }];
+      const transferErrors = await this.client.createTransfers(transfers);
+      console.log(transferErrors);
       return amount;
     }
   }
