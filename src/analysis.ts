d37b149b6f9c9b2c29b3a611d3e4c1235fff50e8
Michiel de Jong <michiel@pondersource.com>
2024-09-27 14:43:27 +0200

    node numbers are strings

--- src/analysis.ts
+++ src/analysis.ts
@@ -7,17 +7,19 @@ const SARAFU_CSV = '../Sarafu2021_UKdb_submission/sarafu_xDAI/sarafu_txns_202001
 const lineReader = createInterface({
   input: createReadStream(SARAFU_CSV),
 });
-const nodes = {};
+const nodes: {
+  [origId: string]: string
+} = {};
 let counter = 0;
 const birdsEyeWorm = new BirdsEyeWorm();
 lineReader.on('line', function (line) {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [ _id,_timeset, transfer_subtype,source,target,weight,_token_name,_token_address ] = line.split(',');
   if (typeof nodes[source] === 'undefined') {
-    nodes[source] = counter++;
+    nodes[source] = (counter++).toString();
   }
   if (typeof nodes[target] === 'undefined') {
-    nodes[target] = counter++;
+    nodes[target] = (counter++).toString();
   }
   if (transfer_subtype === 'STANDARD') {
     birdsEyeWorm.addTransfer(nodes[source], nodes[target], parseFloat(weight));
