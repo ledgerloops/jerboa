882e6b384bea70b5a3f84548ffa5341b43492631
Michiel de Jong <michiel@pondersource.com>
2024-10-01 11:46:05 +0200

    take csv file name from command line

--- src/analysis.ts
+++ src/analysis.ts
@@ -3,7 +3,8 @@ import { createReadStream } from 'fs';
 import { DLD } from './DLD.js';
 
 // const SARAFU_CSV = '../Sarafu2021_UKdb_submission/sarafu_xDAI/sarafu_txns_20200125-20210615.csv';
-const SARAFU_CSV = './__tests__/fixture-3000.csv';
+const SARAFU_CSV = process.argv[2] || './__tests__/fixture-3000.csv';
+console.log('Opening', SARAFU_CSV);
 
 const lineReader = createInterface({
   input: createReadStream(SARAFU_CSV),
