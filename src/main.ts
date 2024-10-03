968bd41c2a19326042045471edd6eded10efa735
Michiel de Jong <michiel@pondersource.com>
2024-10-03 17:30:14 +0200

    able to send messages between workers

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
+import cluster from 'node:cluster';
+import { availableParallelism } from 'node:os';
+import process from 'node:process';
+
 import { Worker } from './Worker.js';
 import { readCsv } from './readCsv.js';
 import { Message } from './Jerboa.js';
 
 // const SARAFU_CSV = '../Sarafu2021_UKdb_submission/sarafu_xDAI/sarafu_txns_20200125-20210615.csv';
 const SARAFU_CSV = process.argv[2] || './__tests__/fixture-3k.csv';
-const WORKER = 0;
-const NUM_WORKERS = 1;
+
+const WORKER = parseInt(process.env.WORKER);
+const NUM_WORKERS = availableParallelism();
 
 function dispatchMessage(from: string, to: string, message: Message): void {
-  const receivingWorker = this.getWorker(parseInt(to));
-  receivingWorker.queueMessageForLocalDelivery(from, to, message);
+  process.send({ from, to, message });
 }
 
 async function runWorker(): Promise<void> {
@@ -31,4 +35,37 @@ async function runWorker(): Promise<void> {
 }
 
 // ...
-runWorker();
+
+
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running, forking ${NUM_WORKERS} threads` + (typeof runWorker).substring(0));
+  // Fork workers.
+  const workers = {};
+  for (let i = 0; i < NUM_WORKERS; i++) {
+    workers[i] = cluster.fork({ WORKER: i });
+    workers[i].on('message', (messageObj) => {
+      console.log('primary received a', messageObj);
+      workers[i].send(`ack ${i}`);
+    });
+  }
+  console.log('sending greetings from primary');
+  for (let i = 0; i < NUM_WORKERS; i++) {
+    workers[i].send(`hi ${i}`);
+  }
+  // cluster.on('messageFromWorker', () => {
+  //   const receivingWorker
+  //   workers[3].send(msg);
+  // })
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} exited with code ${code} and signal ${signal}`);
+  });
+} else {
+  process.on('message', (msg) => {
+    console.log(`Worker ${WORKER} received message`, msg);
+    process.send(msg);
+    // const receivingWorker = this.getWorker(parseInt(to));
+    // receivingWorker.queueMessageForLocalDelivery(from, to, message);
+  });
+  console.log(`Worker ${WORKER} started with pid  ${process.pid}`);
+  process.send({ foo: `Worker ${WORKER} is alive` });
+}
