cfb455ce59a750d0640c5b26f0c54903d92222b5
Michiel de Jong <michiel@pondersource.com>
2024-10-03 16:49:27 +0200

    readCsv with callback

--- __tests__/DLD.test.ts
+++ __tests__/DLD.test.ts
@@ -1,46 +1,13 @@
-import { createInterface } from 'readline';
-import { createReadStream } from 'fs';
+
 import { DLD } from '../src/DLD.js';
+import { readCsv } from '../src/readCsv.js';
 
 const SARAFU_CSV = './__tests__/fixture-300.csv';
-
-async function readCsv(callback: (from: string, to: string, amount: number) => void): Promise<void> {
-  return new Promise((resolve, reject) => {
-    try {
-      const lineReader = createInterface({
-        input: createReadStream(SARAFU_CSV),
-      });
-      const nodes: {
-        [origId: string]: string
-      } = {};
-      let counter = 0;
-      lineReader.on('line', function (line) {
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const [ _id,_timeset, transfer_subtype,source,target,weight,_token_name,_token_address ] = line.split(',');
-        if (typeof nodes[source] === 'undefined') {
-          nodes[source] = (counter++).toString();
-        }
-        if (typeof nodes[target] === 'undefined') {
-          nodes[target] = (counter++).toString();
-        }
-        if (transfer_subtype === 'STANDARD') {
-          callback(nodes[source], nodes[target], parseFloat(weight));
-        }
-      });
-      
-      lineReader.on('close', function () {
-        resolve();
-      });
-    } catch (e) {
-      reject(e);
-    }
-  });
-}
 
 describe('DLD', () => {
   it('finds loops', async () => {
     const dld = new DLD();
-    await readCsv((source: string, target: string, weight: number) => {
+    await readCsv(SARAFU_CSV, (source: string, target: string, weight: number) => {
       // console.log('getting worker', source, parseInt(source));
       const worker = dld.getWorker(parseInt(source));
       worker.addWeight(source, target, weight);
--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,24 @@
-import { createInterface } from 'readline';
-import { createReadStream } from 'fs';
 import { DLD } from './DLD.js';
+import { readCsv } from './readCsv.js';
 
 // const SARAFU_CSV = '../Sarafu2021_UKdb_submission/sarafu_xDAI/sarafu_txns_20200125-20210615.csv';
 const SARAFU_CSV = process.argv[2] || './__tests__/fixture-3k.csv';
-console.log('Opening', SARAFU_CSV);
 
-const lineReader = createInterface({
-  input: createReadStream(SARAFU_CSV),
-});
-const nodes: {
-  [origId: string]: string
-} = {};
-let counter = 0;
-let totalTransAmount = 0;
-let numTrans = 0;
-const dld = new DLD();
-lineReader.on('line', function (line) {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [ _id,_timeset, transfer_subtype,source,target,weight,_token_name,_token_address ] = line.split(',');
-  if (typeof nodes[source] === 'undefined') {
-    nodes[source] = (counter++).toString();
-  }
-  if (typeof nodes[target] === 'undefined') {
-    nodes[target] = (counter++).toString();
-  }
-  if (transfer_subtype === 'STANDARD') {
-    dld.getWorker(parseInt(nodes[source])).addWeight(nodes[source], nodes[target], parseFloat(weight));
+async function run(): Promise<void> {
+  let totalTransAmount = 0;
+  let numTrans = 0;
+  const dld = new DLD();
+  await readCsv(SARAFU_CSV, (source: string, target: string, weight: number) => {
+    dld.getWorker(parseInt(source)).addWeight(source, target, weight);
     numTrans++;
-    totalTransAmount += parseFloat(weight);
-  }
-});
-
-lineReader.on('close', function () {
+    totalTransAmount += weight;
+  });
   console.log(`${numTrans} primary transfers with value of ${totalTransAmount} done, now inviting bilateral netting`);
   dld.runAllTasks();
   console.log('bilateral netting done, now inviting probes');
   dld.runAllWorms();
   console.log('done');
-});
+}
+
+// ...
+run();
