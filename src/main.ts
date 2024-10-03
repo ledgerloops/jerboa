a4b1431f7f178d57c65aa6ed7d97acdbe067b40a
Michiel de Jong <michiel@pondersource.com>
2024-10-03 17:00:44 +0200

    run per worker in main function

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,34 @@
-import { DLD } from './DLD.js';
+import { Worker } from './Worker.js';
 import { readCsv } from './readCsv.js';
+import { Message } from './Jerboa.js';
 
 // const SARAFU_CSV = '../Sarafu2021_UKdb_submission/sarafu_xDAI/sarafu_txns_20200125-20210615.csv';
 const SARAFU_CSV = process.argv[2] || './__tests__/fixture-3k.csv';
+const WORKER = 0;
+const NUM_WORKERS = 1;
 
-async function run(): Promise<void> {
-  let totalTransAmount = 0;
+function dispatchMessage(from: string, to: string, message: Message): void {
+  const receivingWorker = this.getWorker(parseInt(to));
+  receivingWorker.queueMessageForLocalDelivery(from, to, message);
+}
+
+async function runWorker(): Promise<void> {
   let numTrans = 0;
-  const dld = new DLD();
-  await readCsv(SARAFU_CSV, (source: string, target: string, weight: number) => {
-    dld.getWorker(parseInt(source)).addWeight(source, target, weight);
-    numTrans++;
-    totalTransAmount += weight;
+  let totalTransAmount = 0;
+  const worker = new Worker(WORKER, NUM_WORKERS, dispatchMessage);
+  await readCsv(SARAFU_CSV, (from: string, to: string, amount: number) => {
+    if (parseInt(from) % NUM_WORKERS === WORKER) {
+      worker.addWeight(from, to, amount);
+      numTrans++;
+      totalTransAmount += amount;
+    }
   });
-  console.log(`${numTrans} primary transfers with value of ${totalTransAmount} done, now inviting bilateral netting`);
-  dld.runAllTasks();
-  console.log('bilateral netting done, now inviting probes');
-  dld.runAllWorms();
-  console.log('done');
+  console.log(`[WORKER ${WORKER}] ${numTrans} primary transfers with value of ${totalTransAmount} done, now inviting bilateral netting`);
+  worker.runTasks();
+  console.log(`WORKER ${WORKER}] bilateral netting done, now inviting probes`);
+  worker.runWorm();
+  console.log(`WORKER ${WORKER}] done`);
 }
 
 // ...
-run();
+runWorker();
