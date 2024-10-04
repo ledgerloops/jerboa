837d1d2c79b7f5fc14f6ee3a669d811311c66d80
Michiel de Jong <michiel@pondersource.com>
2024-10-04 14:46:39 +0200

    Use silence timers

--- src/Cluster.ts
+++ src/Cluster.ts
@@ -6,6 +6,7 @@ import { Message } from './Jerboa.js';
 export class Cluster {
   private filename: string;
   private numWorkers: number;
+  private lastMessageSeen: number;
   constructor(filename: string, numWorkers: number) {
     this.filename = filename;
     this.numWorkers = numWorkers;
@@ -14,19 +15,12 @@ export class Cluster {
     console.log(`Primary ${process.pid} is running, forking ${this.numWorkers} threads`);
     // Fork workers.
     const workers = {};
-    let forwardMessage;
-    const done = new Promise(resolve => {
-      let timeout;
-      timeout = setTimeout(resolve, 10000);
-      forwardMessage = (messageObj: { from: string, to: string, message: object }): void => {
-        const recipientWorker = parseInt(messageObj.to) % this.numWorkers;
-        // console.log('primary forwards', messageObj, `to worker ${recipientWorker}`);
-        clearTimeout(timeout);
-        timeout = setTimeout(resolve, 1000);
-        // console.log('primary will finish in 1000ms from now');
-        workers[recipientWorker].send(messageObj);
-      };
-    });
+    const forwardMessage = (messageObj: { from: string, to: string, message: object }): void => {
+      const recipientWorker = parseInt(messageObj.to) % this.numWorkers;
+      console.log('primary forwards', messageObj, `to worker ${recipientWorker}`);
+      this.lastMessageSeen = Date.now();
+      workers[recipientWorker].send(messageObj);
+    };
     for (let i = 0; i <this. numWorkers; i++) {
       workers[i] = cluster.fork({ WORKER: i });
       workers[i].on('message', forwardMessage);
@@ -43,6 +37,31 @@ export class Cluster {
     for (let i = 0; i < this.numWorkers; i++) {
       workers[i].send(`start`);
     }
+    await new Promise(resolve => {
+      const timer = setInterval(() => {
+        const silence = (Date.now() - this.lastMessageSeen);
+        console.log(`Checking silence ${silence}`);
+        if (silence > 1000) {
+          clearInterval(timer);
+          resolve(true);
+        }
+      }, 100);
+    });
+    for (let probeId = 0; probeId < 2; probeId++) {
+      console.log(`Primary sends signal to ${probeId % this.numWorkers} to start probe ${probeId}`);
+      workers[probeId % this.numWorkers].send(`startProbe ${probeId}`);
+      console.log('probe started, waiting for silence again');
+      await new Promise(resolve => {
+        const timer = setInterval(() => {
+          const silence = (Date.now() - this.lastMessageSeen);
+          console.log(`Checking silence ${silence}`);
+          if (silence > 1000) {
+            clearInterval(timer);
+            resolve(true);
+          }
+        }, 100);
+      });
+    }
     cluster.on('exit', (worker, code, signal) => {
       if (signal) {
         console.log(`worker ${worker.id} was killed by signal: ${signal}`);
@@ -52,7 +71,6 @@ export class Cluster {
         console.log(`worker ${worker.id} success!`);
       }
     });
-    await done;
     for (let i = 0; i <this. numWorkers; i++) {
       workers[i].send('shutdown');
       workers[i].disconnect();
@@ -69,14 +87,31 @@ export class Cluster {
     });
     return new Promise((resolve) => {
       process.on('message', async (msg) => {
-        // console.log(`Worker ${workerNo} received message from primary`, msg);
-        if (msg === `start`) {
-          worker.readTransfersFromCsv(this.filename);
-        } else if (msg === `shutdown`) {
-          console.log(`Worker ${workerNo} received shutdown message from primary`);
-          worker.teardown();
-          resolve(42);
+        if (typeof msg === 'string') {
+          console.log(`Worker ${workerNo} received message of string type`, msg);
+          const parts = msg.split(' ');
+          switch(parts[0]) {
+            case `start`: {
+              worker.readTransfersFromCsv(this.filename);
+              break;
+            }
+            case `shutdown`: {
+              console.log(`Worker ${workerNo} received shutdown message from primary`);
+              worker.teardown();
+              resolve(42);
+              break;
+            }
+            case `startProbe`: {
+              console.log(`Worker ${workerNo} starts worm ${parts[1]}`);
+              worker.runOneWorm(parseInt(parts[1]));
+              break;
+            }
+            default: {
+              console.log('unknown string message from primary', msg);
+            }
+          }
         } else {
+          console.log(`Worker ${workerNo} received message of non-string type`, msg);
           const { from, to, message } = msg as { from: string, to: string, message: Message };
           worker.queueMessageForLocalDelivery(from, to, message);
         }
